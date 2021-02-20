0f9f4a2d8b7522ddf855e1c929c6238d7623d866
jkursula <tkursula@gmail.com>
2021-02-20 09:23:06 +0200

    still borken test

--- Database/test_database.py
+++ Database/test_database.py
@@ -1,43 +1,19 @@
 import os
+import random
 import pytest
 import tempfile
 
-import app
+# import app
 from datetime import datetime
 
-from app import Transaction, Category, User, BankAccount
+
+from app import app , db, Transaction, Category, User, BankAccount
 from sqlalchemy.engine import Engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import event
 
-##TODO: find out about using foreign keys
+##TODO: db_test.py does not work either...
 
-
-def _get_transaction(price=999999999.9, dateTime=datetime.now(), sender=1, receiver=1, category=1):
-    return Transaction(
-        price=price,
-        dateTime=dateTime,
-        sender=sender,
-        receiver=receiver,
-        category=category
-    )
-
-def _get_category():
-    return Category(
-        categoryName="cat"
-    )
-    
-def _get_user(number=1):
-    return User(
-        username="user" + str(number),
-        password="password"
-    )
-    
-def _get_bankAccount(number=1):
-    return BankAccount(
-        iban="FI000000000000" + str(number),
-        bankName="The bank"
-    )
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -48,58 +24,91 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 @pytest.fixture
 def db_handle():
     db_fd, db_fname = tempfile.mkstemp()
-    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
-    app.app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
+    app.config["TESTING"] = True
     
-    with app.app.app_context():
-        app.db.create_all()
+    with app.app_context():
+        db.create_all()
         
-    yield app.db
+    yield db
     
-    app.db.session.remove()
+    db.session.remove()
     os.close(db_fd)
     os.unlink(db_fname)
 
+def _get_bankAccount(iban, bankName):
+    return BankAccount(
+        iban=iban,
+        bankName=bankName
+    )
 
-def test_create_bankAccount(db_handle):
-    bankAccountRow = _get_bankAccount()
-    db_handle.session.add(bankAccountRow)
-    db_handle.session.commit()
-    #If condition returns False, AssertionError is raised 
-    assert BankAccount.query.count() == 1
+def _get_user(username, password):
+    return User(
+        username=username,
+        password=password
+    )
+
+def _get_category(name):
+    return Category(
+        categoryName=name
+    )
 
 
-def test_create_user(db_handle):
-    bankAccountRow = _get_bankAccount()
-    user = _get_user()
-    db_handle.session.add(bankAccountRow)
-    db_handle.session.add(user)
-    db_handle.session.commit()
-    #If condition returns False, AssertionError is raised 
-    assert User.query.count() == 1
+def _get_transaction(price, dateTime, sender, receiver, category):
+    return Transaction(
+        price=price,
+        dateTime=dateTime,
+        sender=sender,
+        receiver=receiver,
+        category=category
+    )
 
-def test_create_category(db_handle):
-    category = _get_category()
-    db_handle.session.add(category)
-    db_handle.session.commit()
-    #If condition returns False, AssertionError is raised 
-    assert Category.query.count() == 1
 
-def test_create_transaction(db_handle):
-    bankAccountRow1 = _get_bankAccount(1)
-    bankAccountRow2 = _get_bankAccount(2)
-    user1 = _get_user(1)
-    user2 = _get_user(2)
-    db_handle.session.add(bankAccountRow1)
-    db_handle.session.add(bankAccountRow2)
+def test_create_instances(db_handle):
+    """
+    Tests that we can create one instance of each model and save them to the
+    database using valid values for all columns. After creation, test that 
+    everything can be found from database, and that all relationships have been
+    saved correctly.
+    """
+
+    # Create everything
+    bankAccount1 = _get_bankAccount(iban="FI01", bankName="The bank")
+    bankAccount2 = _get_bankAccount(iban="FI02", bankName="The bank")
+    user1 = _get_user(username="user1", password="password")
+    user2 = _get_user(username="user2", password="password")
+    user1.bankAccount = bankAccount1
+    user2.bankAccount = bankAccount2
+    category1 = _get_category(name="cat1")
+    category2 = _get_category(name="cat2")
+    transaction = _get_transaction(price=3.50, dateTime=datetime.now(), sender=user1, receiver=user2, category=category1)
+    
+    db_handle.session.add(bankAccount1)
+    db_handle.session.add(bankAccount2)
     db_handle.session.add(user1)
     db_handle.session.add(user2)
-    db_handle.session.commit()
-    transaction = _get_transaction()
+    db_handle.session.add(category1)
+    db_handle.session.add(category2)
     db_handle.session.add(transaction)
     db_handle.session.commit()
-    #If condition returns False, AssertionError is raised 
+    
+    # Check that everything exists
+    assert BankAccount.query.count() == 2
+    assert User.query.count() == 2
+    assert Category.query.count() == 2
     assert Transaction.query.count() == 1
+    db_bankAccounts = BankAccount.query.all()
+    db_users = User.query.all()
+
+    db_categories = Category.query.all()
+    db_transaction = Transaction.query.first()
+    
+    # Check all relationships (both sides)
+
+    assert db_transaction.sender in db_users
+    assert db_transaction.receiver in db_users
+    assert db_transaction.category in db_categories
+    assert db_transaction.sender.bankAccount in db_bankAccounts
 
 
 # def test_create_instances(db_handle):
