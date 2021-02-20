c07579c22b8e741b307e15dd648bf7dd2977eb10
essipassoja <44393175+essipassoja@users.noreply.github.com>
2021-02-20 09:44:42 +0200

    Working version of test_database.py

--- Database/test_database.py
+++ Database/test_database.py
@@ -6,11 +6,11 @@ import tempfile
 # import app
 from datetime import datetime
 
-
-from app import app , db, Transaction, Category, User, BankAccount
+from app import app, db, Transaction, Category, User, BankAccount
 from sqlalchemy.engine import Engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import event
+
 
 ##TODO: db_test.py does not work either...
 
@@ -21,20 +21,22 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
+
 @pytest.fixture
 def db_handle():
     db_fd, db_fname = tempfile.mkstemp()
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
     app.config["TESTING"] = True
-    
+
     with app.app_context():
         db.create_all()
-        
+
     yield db
-    
+
     db.session.remove()
     os.close(db_fd)
     os.unlink(db_fname)
+
 
 def _get_bankAccount(iban, bankName):
     return BankAccount(
@@ -42,11 +44,13 @@ def _get_bankAccount(iban, bankName):
         bankName=bankName
     )
 
+
 def _get_user(username, password):
     return User(
         username=username,
         password=password
     )
+
 
 def _get_category(name):
     return Category(
@@ -67,7 +71,7 @@ def _get_transaction(price, dateTime, sender, receiver, category):
 def test_create_instances(db_handle):
     """
     Tests that we can create one instance of each model and save them to the
-    database using valid values for all columns. After creation, test that 
+    database using valid values for all columns. After creation, test that
     everything can be found from database, and that all relationships have been
     saved correctly.
     """
@@ -76,13 +80,14 @@ def test_create_instances(db_handle):
     bankAccount1 = _get_bankAccount(iban="FI01", bankName="The bank")
     bankAccount2 = _get_bankAccount(iban="FI02", bankName="The bank")
     user1 = _get_user(username="user1", password="password")
-    user2 = _get_user(username="user2", password="password")
-    user1.bankAccount = bankAccount1
-    user2.bankAccount = bankAccount2
+    user2 = _get_user(username="user2", password="password2")
+    user1.bankAccount.append(bankAccount1)
+    user2.bankAccount.append(bankAccount2)
     category1 = _get_category(name="cat1")
     category2 = _get_category(name="cat2")
-    transaction = _get_transaction(price=3.50, dateTime=datetime.now(), sender=user1, receiver=user2, category=category1)
-    
+    transaction = _get_transaction(price=3.50, dateTime=datetime.now(), sender=user1, receiver=user2,
+                                   category=[category1])
+
     db_handle.session.add(bankAccount1)
     db_handle.session.add(bankAccount2)
     db_handle.session.add(user1)
@@ -91,7 +96,7 @@ def test_create_instances(db_handle):
     db_handle.session.add(category2)
     db_handle.session.add(transaction)
     db_handle.session.commit()
-    
+
     # Check that everything exists
     assert BankAccount.query.count() == 2
     assert User.query.count() == 2
@@ -102,15 +107,15 @@ def test_create_instances(db_handle):
 
     db_categories = Category.query.all()
     db_transaction = Transaction.query.first()
-    
+
     # Check all relationships (both sides)
 
     assert db_transaction.sender in db_users
     assert db_transaction.receiver in db_users
-    assert db_transaction.category in db_categories
-    assert db_transaction.sender.bankAccount in db_bankAccounts
-
+    for category in db_transaction.category:
+        assert category in db_categories
+    for bankAccount in db_transaction.sender.bankAccount:
+        assert bankAccount in db_bankAccounts
 
 # def test_create_instances(db_handle):
 
-    
