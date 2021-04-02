266d1cb76db58af407d79bdb9cc91cf54961281d
teikaval <teemu.ikavalko@gmail.com>
2021-04-02 13:52:55 +0300

    Added bank_account stuff

--- budgethub/resources/bank_account.py
+++ budgethub/resources/bank_account.py
@@ -12,17 +12,119 @@ from budgethub.utils import *
 #Bank account resources
 class BankAccountCollection(Resource):
     def get(self):
-        pass
+        body = BankAccountBuilder()
+        body.add_namespace("bumeta", LINK_RELATIONS_URL)
+        body.add_control("self", "/api/bankaccount/")
+        body.add_control_add_bank_account()
+
+        banks = []
+        for bank in BankAccount.query.all():
+            bank_item_body = BankAccountBuilder(
+                iban = bank.iban,
+                bankName = bank.bankName
+
+            )
+            bank_item_body.add_control("self", url_for("api.bankaccountitem", iban=bank.iban))
+            bank_item_body.add_control("profile", BANK_ACOUNT_PROFILE)
+            banks.append(bank_item_body)
+        body["items"] = banks
+
+        return Response(json.dumps(body), 200, mimetype=MASON)
 
     def post(self):
-        pass
+        if not request.json:
+            return create_error_response(
+                415, "Unsupported media type",
+                "Requests must be JSON"
+            )
+
+        try:
+            validate(request.json, BankAccountBuilder.bank_account_schema())
+        except ValidationError as e:
+            return create_error_response(400, "Invalid JSON document", str(e))
+
+        bank = BankAccount(
+            iban=request.json["iban"],
+            BankName=request.json["BankName"]
+ 
+        )
+
+        try:
+            db.session.add(bank)
+            db.session.commit()
+        except IntegrityError:
+            return create_error_response(
+                409, "Already exists",
+                "Bankaccount with iban '{}' already exists.".format(request.json["iban"])
+            )
+
+        return Response(status=201, headers={
+            "Location": url_for("api.bankaccountitem", iban=request.json["iban"])
+        })
 
 class BankAccountItem(Resource):
-    def get(self):
-        pass
+    def get(self, iban):
+        db_bank = BankAccount.query.filter_by(iban=iban).first()
+        if db_bank is None:
+            return create_error_response(
+                404, "Not found",
+                "No Bankaccount was found with the iban {}".format(iban)
+            )
 
-    def put(self):
-        pass
+        body = BankAccountBuilder(
+                iban = db_bank.iban,
+                BankName = db_bank.BankName
+            )
+        body.add_namespace("bumeta", LINK_RELATIONS_URL)
+        body.add_control("self", url_for("api.bankaccountitem", iban=iban))
+        body.add_control("profile", BANK_ACCOUNT_PROFILE)
+        body.add_control("bumeta:banks-all", url_for("api.bankaccountcollection"))
+        body.add_control_delete_bank_account(iban)
+        body.add_control_edit_bank_account(iban)
 
-    def delete(self):
-        pass
+        return Response(json.dumps(body), 200, mimetype=MASON)
+
+    def put(self, iban):
+        db_bank = BankAccount.query.filter_by(iban=iban).first()
+        if db_bank is None:
+            return create_error_response(
+                404, "Not found",
+                "No bankaccount was found with the iban {}".format(iban)
+            )
+
+        if not request.json:
+            return create_error_response(
+                415, "Unsupported media type",
+                "Requests must be JSON"
+            )
+
+        try:
+            validate(request.json, BankAccountBuilder.bank_account_schema())
+        except ValidationError as e:
+            return create_error_response(400, "Invalid JSON document", str(e))
+
+        db_bank.iban = request.json["iban"]
+        db_bank.bankName = request.json["bankName"]
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            return create_error_response(
+                409, "Already exists",
+                "Bankaccount with iban '{}' already exists.".format(request.json["iban"])
+            )
+
+        return Response(status=204)
+
+    def delete(self, iban):
+        db_bank = BankAccount.query.filter_by(iban=iban).first()
+        if db_bank is None:
+            return create_error_response(
+                404, "Not found",
+                "No Bankaccount was found with the iban {}".format(iban)
+            )
+
+        db.session.delete(db_bank)
+        db.session.commit()
+
+        return Response(status=204)
