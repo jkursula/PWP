3e1a0da82a05504ff008b2d1bf02a685f8ce21b4
teikaval <teemu.ikavalko@gmail.com>
2021-04-08 07:17:13 +0300

    added tests for user collection

--- tests/test_api.py
+++ tests/test_api.py
@@ -102,6 +102,13 @@ def _get_category_json(category_name="cat3"):
     '''
     
     return {"category_name":"{}".format(category_name), "transaction":["1"]}
+
+def _get_user_json(username="user3"):
+    """
+    Creates a valid bankaccount JSON object to be used for PUT and POST tests.
+    """
+    
+    return {"username":"{}".format(username), "password":"En kerro", "bankAccount":["FI01"]}
     
 def _check_namespace(client, response):
     """
@@ -181,6 +188,29 @@ def _check_category_control_put_method(ctrl, client, obj):
     validate(body, schema)
     resp = client.put(href, json=body)
     assert resp.status_code == 204
+
+def _check_user_control_put_method(ctrl, client, obj):
+    """
+    Checks a PUT type control from a JSON object be it root document or an item
+    in a collection. In addition to checking the "href" attribute, also checks
+    that method, encoding and schema can be found from the control. Also
+    validates a valid sensor against the schema of the control to ensure that
+    they match. Finally checks that using the control results in the correct
+    status code of 204.
+    """
+    
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "put"
+    assert encoding == "json"
+    body = _get_user_json()
+    body["username"] = obj["username"]
+    validate(body, schema)
+    resp = client.put(href, json=body)
+    assert resp.status_code == 204
     
 def _check_control_post_method(ctrl, client, obj):
     """
@@ -223,6 +253,28 @@ def _check_category_control_post_method(ctrl, client, obj):
     assert method == "post"
     assert encoding == "json"
     body = _get_category_json()
+    validate(body, schema)
+    resp = client.post(href, json=body)
+    assert resp.status_code == 201
+    
+def _check_user_control_post_method(ctrl, client, obj):
+    """
+    Checks a POST type control from a JSON object be it root document or an item
+    in a collection. In addition to checking the "href" attribute, also checks
+    that method, encoding and schema can be found from the control. Also
+    validates a valid sensor against the schema of the control to ensure that
+    they match. Finally checks that using the control results in the correct
+    status code of 201.
+    """
+    
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "post"
+    assert encoding == "json"
+    body = _get_user_json()
     validate(body, schema)
     resp = client.post(href, json=body)
     assert resp.status_code == 201
@@ -288,7 +340,7 @@ class TestBankaccountCollection(object):
         assert resp.status_code == 400
         
         
-class TestSensorItem(object):
+class TestBankaccountItem(object):
     
     RESOURCE_URL = "/api/bankaccounts/FI01/"
     INVALID_URL = "/api/bankaccounts/XD/"
@@ -491,6 +543,147 @@ class TestCategoryItem(object):
         assert resp.status_code == 200
         body = json.loads(resp.data)
         assert body["category_name"] == valid["category_name"]
+        
+    def test_delete(self, client):
+        """
+        Tests the DELETE method. Checks that a valid request reveives 204
+        response and that trying to GET the sensor afterwards results in 404.
+        Also checks that trying to delete a sensor that doesn't exist results
+        in 404.
+        """
+        
+        resp = client.delete(self.RESOURCE_URL)
+        assert resp.status_code == 204
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 404
+        resp = client.delete(self.INVALID_URL)
+        assert resp.status_code == 404
+        
+class TestUserCollection(object):
+    """
+    This class implements tests for each HTTP method in Bankaccount collection
+    resource. 
+    """
+    
+    RESOURCE_URL = "/api/users/"
+
+    def test_get(self, client):
+        """
+        Tests the GET method. Checks that the response status code is 200, and
+        then checks that all of the expected attributes and controls are
+        present, and the controls work. Also checks that all of the items from
+        the DB popluation are present, and their controls.
+        """
+        
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        _check_namespace(client, body)
+        _check_user_control_post_method("bumeta:add-user", client, body)
+        assert len(body["items"]) == 2
+        for item in body["items"]:
+            _check_control_get_method("self", client, item)
+            _check_control_get_method("profile", client, item)
+            assert "username" in item
+            #assert "password" in item
+
+    def test_post(self, client):
+        """
+        Tests the POST method. Checks all of the possible error codes, and 
+        also checks that a valid request receives a 201 response with a 
+        location header that leads into the newly created resource.
+        """
+        
+        valid = _get_user_json()
+        
+        # test with wrong content type
+        resp = client.post(self.RESOURCE_URL, data=json.dumps(valid))
+        assert resp.status_code == 415
+        
+        # test with valid and see that it exists afterward
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 201
+        assert resp.headers["Location"].endswith(self.RESOURCE_URL + valid["username"] + "/")
+        resp = client.get(resp.headers["Location"])
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["username"] == "user3"
+        assert body["bankAccount"] == ["FI01"]
+        
+        # send same data again for 409
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+        
+        # remove iban field for 400
+        valid.pop("username")
+        resp = client.post(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
+        
+        
+class TestUserItem(object):
+    
+    RESOURCE_URL = "/api/users/user1/"
+    INVALID_URL = "/api/users/XD/"
+    MODIFIED_URL = "/api/users/user3/"
+    
+    def test_get(self, client):
+        """
+        Tests the GET method. Checks that the response status code is 200, and
+        then checks that all of the expected attributes and controls are
+        present, and the controls work. Also checks that all of the items from
+        the DB popluation are present, and their controls.
+        """
+
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["username"] == "user1"
+        assert body["bankAccount"] == ["FI01"]
+        _check_namespace(client, body)
+        _check_control_get_method("profile", client, body)
+        _check_control_get_method("bumeta:users-all", client, body)
+        _check_user_control_put_method("edit", client, body)
+        _check_control_delete_method("bumeta:delete", client, body)
+        resp = client.get(self.INVALID_URL)
+        assert resp.status_code == 404
+
+    def test_put(self, client):
+        """
+        Tests the PUT method. Checks all of the possible erroe codes, and also
+        checks that a valid request receives a 204 response. Also tests that
+        when name is changed, the sensor can be found from a its new URI. 
+        """
+        
+        valid = _get_user_json()
+        
+        # test with wrong content type
+        resp = client.put(self.RESOURCE_URL, data=json.dumps(valid))
+        assert resp.status_code == 415
+        
+        resp = client.put(self.INVALID_URL, json=valid)
+        assert resp.status_code == 404
+        
+        # test with another bankaccounts iban 
+        valid["username"] = "user2"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+        
+        '''# test with valid (only change model)
+        valid["password"] = "New password"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 204'''
+        
+        # remove field for 400
+        valid.pop("username")
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
+        
+        valid = _get_user_json()
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        resp = client.get(self.MODIFIED_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        assert body["username"] == valid["username"]
         
     def test_delete(self, client):
         """
